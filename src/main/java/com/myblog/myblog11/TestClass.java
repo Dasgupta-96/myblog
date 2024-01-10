1c24346a0f0df54a53fcd81f1365a4f4808afcbb
Dasgupta-96 <arkadeb55@gmail.com>
2024-01-10 17:47:04 +0530

    using Stream Api Filter() method.

--- src/main/java/com/myblog/myblog11/TestClass.java
+++ src/main/java/com/myblog/myblog11/TestClass.java
@@ -10,33 +10,46 @@ import java.net.Inet4Address;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.StringTokenizer;
 import java.util.function.DoubleToIntFunction;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class TestClass {
     public static void main(String[] args) {
+//
+List<Integer> data1 = Arrays.asList(10,15,18,65,42);
+List<Integer> result1= data1.stream().filter(x->x.toString().startsWith("1")).collect(Collectors.toList());
+        System.out.println(result1);
 
-        // Function functional interface -> it takes input and provides output
-        // map() is a method which takes function functional interface, we cant
 
-       Function<String,Integer> data =  str->str.length();
-       Integer a = data.apply("aryan");
-        System.out.println(a);
+//// Filter out the String word "Java" in the given word lists
+  List<String> str = Arrays.asList("Java is a language","C++ is oop's language", "python is very easy", "JavaScript is backdated");
+ List<String> res =  str.stream().filter(s->!s.contains("Java")).collect(Collectors.toList());
 
-       Function<Integer, Integer> f = i->i+10;
-      Integer  x =  f.apply(50);
-        System.out.println(x);
+        System.out.println(res);
 
-        List<String> names= Arrays.asList("arka","deb","mike","ram");
-       List<String> name=  names.stream().map(s->s.toUpperCase()).collect(Collectors.toList());
+//// word starts with letter 'a':
+ List<String> data = Arrays.asList("deb", "arka", "roy");
+ List<String> result= data.stream().filter(s->s.startsWith("r")).collect(Collectors.toList());
+
+        System.out.println(result);
+
+//Filter out names that have exactly four letters:
+
+  List<String> names=  Arrays.asList("john","ram", "swagata", "elephant");
+  List<String> name = names.stream().filter(n->n.length()==3).collect(Collectors.toList());
+
         System.out.println(name);
 
-       List<Integer> val =  Arrays.asList(10,20,30);
-      List<Integer> sum =  val.stream().map(i->i+10).collect(Collectors.toList());
-        System.out.println(sum);
 
+// Filter out the nos greater than 50:
 
+       List<Integer> no =  Arrays.asList(78,56,10,19,45);
+     List<Integer> r = no.stream().filter(n->n<=50).collect(Collectors.toList());
+        System.out.println(r);
     }
+
+
 
 }
