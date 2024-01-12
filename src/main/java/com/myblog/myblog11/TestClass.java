733092be39fb679cfcb2b472d43b7c0d11c33f64
Dasgupta-96 <arkadeb55@gmail.com>
2024-01-12 16:32:10 +0530

    using Stream Api Consumer() method.

--- src/main/java/com/myblog/myblog11/TestClass.java
+++ src/main/java/com/myblog/myblog11/TestClass.java
@@ -1,9 +1,11 @@
 package com.myblog.myblog11;
 
 
+import ch.qos.logback.core.net.server.ConcurrentServerRunner;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import org.hibernate.persister.entity.SingleTableEntityPersister;
 import org.springframework.cglib.core.internal.Function;
+import org.yaml.snakeyaml.nodes.CollectionNode;
 
 import java.lang.reflect.Array;
 import java.net.Inet4Address;
@@ -11,45 +13,21 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
+import java.util.function.Consumer;
 import java.util.function.DoubleToIntFunction;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class TestClass {
     public static void main(String[] args) {
-//
-List<Integer> data1 = Arrays.asList(10,15,18,65,42);
-List<Integer> result1= data1.stream().filter(x->x.toString().startsWith("1")).collect(Collectors.toList());
-        System.out.println(result1);
+        // Consumer functional interface takes input and produces no output
+      Consumer<String> result=  name-> System.out.println(name);
+        result.accept("debu");
 
+// Example 2:
 
-//// Filter out the String word "Java" in the given word lists
-  List<String> str = Arrays.asList("Java is a language","C++ is oop's language", "python is very easy", "JavaScript is backdated");
- List<String> res =  str.stream().filter(s->!s.contains("Java")).collect(Collectors.toList());
-
-        System.out.println(res);
-
-//// word starts with letter 'a':
- List<String> data = Arrays.asList("deb", "arka", "roy");
- List<String> result= data.stream().filter(s->s.startsWith("r")).collect(Collectors.toList());
-
-        System.out.println(result);
-
-//Filter out names that have exactly four letters:
-
-  List<String> names=  Arrays.asList("john","ram", "swagata", "elephant");
-  List<String> name = names.stream().filter(n->n.length()==3).collect(Collectors.toList());
-
-        System.out.println(name);
-
-
-// Filter out the nos greater than 50:
-
-       List<Integer> no =  Arrays.asList(78,56,10,19,45);
-     List<Integer> r = no.stream().filter(n->n<=50).collect(Collectors.toList());
-        System.out.println(r);
+      List<String> names=  Arrays.asList("ram", "sam", "rita");
+      Consumer<String> val = name-> System.out.println(name);
+      names.forEach(val);
     }
-
-
-
 }
