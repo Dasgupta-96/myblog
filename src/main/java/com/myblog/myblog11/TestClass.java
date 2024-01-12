d046df88cb28674099090f6bba798e193a6d8cb2
Dasgupta-96 <arkadeb55@gmail.com>
2024-01-12 16:54:53 +0530

    Consumer()& Supplier Interface.

--- src/main/java/com/myblog/myblog11/TestClass.java
+++ src/main/java/com/myblog/myblog11/TestClass.java
@@ -9,18 +9,16 @@ import org.yaml.snakeyaml.nodes.CollectionNode;
 
 import java.lang.reflect.Array;
 import java.net.Inet4Address;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.DoubleToIntFunction;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 public class TestClass {
     public static void main(String[] args) {
-        // Consumer functional interface takes input and produces no output
+//         Consumer functional interface takes input and produces no output
       Consumer<String> result=  name-> System.out.println(name);
         result.accept("debu");
 
@@ -29,5 +27,11 @@ public class TestClass {
       List<String> names=  Arrays.asList("ram", "sam", "rita");
       Consumer<String> val = name-> System.out.println(name);
       names.forEach(val);
+
+// Supplier functional interface doesn't take input, it produces output only
+
+      Supplier<Integer> val1=   ()-> new Random().nextInt(50);
+    Integer res=   val1.get();
+        System.out.println(res);
     }
 }
